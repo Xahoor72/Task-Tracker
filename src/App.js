b75ab65ddde6bc455abfa9b60d4b6e059f7ba50b
Zahoor Ahmad Shergujry <85664053+Xahoor72@users.noreply.github.com>
2024-07-17 12:03:36 +0530

    added live chat

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 // App.js
-import React from 'react';
+import React ,{useEffect} from 'react';
+
 import TaskList from './components/TaskList';
+const TawkToScript = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://embed.tawk.to/6697641632dca6db2cb1111a/1i2vltv1d';
+    script.charset = 'UTF-8';
+    script.setAttribute('crossorigin', '*');
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return null;
+};
 
 function App() {
   return (
+    
     <div className="App">
+    
       <TaskList />
+      <TawkToScript />
     </div>
   );
 }
